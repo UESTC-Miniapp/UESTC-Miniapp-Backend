9ceef066229c603b1341be49a0dd9da75ca783f1
黄ZY <huangziyi010@live.cn>
2019-02-14 12:54:16 +0800

    update: 更新数据库文件

--- res/wechat.sql
+++ res/wechat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 2018-12-11 11:20:08
+-- Generation Time: 2019-02-14 12:53:31
 -- 服务器版本： 10.0.34-MariaDB-0ubuntu0.16.04.1
 -- PHP Version: 7.0.32-0ubuntu0.16.04.1
 
@@ -25,6 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `errlog`
+--
+
+CREATE TABLE `errlog` (
+  `msg` text,
+  `err_code` int(11) DEFAULT NULL,
+  `serial` bigint(20) UNSIGNED NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `stdlog`
+--
+
+CREATE TABLE `stdlog` (
+  `msg` text,
+  `serial` bigint(20) UNSIGNED NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `user_info`
 --
 
@@ -42,10 +69,42 @@ CREATE TABLE `user_info` (
 --
 
 --
+-- Indexes for table `errlog`
+--
+ALTER TABLE `errlog`
+  ADD PRIMARY KEY (`serial`),
+  ADD UNIQUE KEY `serial` (`serial`),
+  ADD UNIQUE KEY `errlog_serial_uindex` (`serial`);
+
+--
+-- Indexes for table `stdlog`
+--
+ALTER TABLE `stdlog`
+  ADD PRIMARY KEY (`serial`),
+  ADD UNIQUE KEY `serial` (`serial`),
+  ADD UNIQUE KEY `stdlog_serial_uindex` (`serial`);
+
+--
 -- Indexes for table `user_info`
 --
 ALTER TABLE `user_info`
   ADD UNIQUE KEY `student_number` (`student_number`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `errlog`
+--
+ALTER TABLE `errlog`
+  MODIFY `serial` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
+-- 使用表AUTO_INCREMENT `stdlog`
+--
+ALTER TABLE `stdlog`
+  MODIFY `serial` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=665;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
